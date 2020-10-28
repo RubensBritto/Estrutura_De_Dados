d020be57813c80cdb298eb5adfe53b5f148da8bb
RubensBritto <jrsb@ic.ufal.br>
2020-10-27 21:22:17 -0300

    first update

--- Pilha/main.py
+++ Pilha/main.py
@@ -1,15 +1,42 @@
 # -*- coding: utf-8 -*-
 # Usando módulo interno do python para ler arquivos em ssv
 import csv
-dados=[]
-with open('datas/2015.csv', newline='') as arquivo:
-    leitor=csv.reader(arquivo)
-    leitor.__next__()
-    for linha in leitor:
-        dados.append(linha)
+import random
+from pilha import Stack
 
-# Aqui posso imprimir qualquer linha da tabela
-print(len(dados))
+dados = []
+pilha =  Stack()
 
-# Ou qualquer coluna da linha
-#print(dados[5][0])
+def openData():
+    with open('datas/2015.csv', newline='') as arquivo:
+        leitor=csv.reader(arquivo)
+        leitor.__next__()
+        for linha in leitor:
+            dados.append(linha)
+
+def aleatorioData():
+    k = 0
+    visitados = []
+    valorAleatorio = random.randint(1,100)
+    while(k < 100):
+        if valorAleatorio not in visitados:
+            visitados.append(valorAleatorio)
+            print(dados[valorAleatorio])
+            pilha.push(dados[valorAleatorio])
+            k+=1
+        
+'''
+def showList():
+    for i in range(len(dados)):
+        rint(pilha.)
+'''
+def main():
+    openData()
+    aleatorioData()
+    print(pilha.size())
+    #showList()
+    #print(len(dados))
+
+
+if __name__ == "__main__":
+    main()
