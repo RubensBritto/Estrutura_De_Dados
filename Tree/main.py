ea6a536e4f6e8cd74a4d28c797cf85d71729d607
PomboObeso <matheus.gomes201@gmail.com>
2020-11-22 20:35:10 -0300

    Updates (sem teste kkk) na Tree3

--- Tree/main.py
+++ Tree/main.py
@@ -3,12 +3,15 @@
 from Tree.tree import BinaryTree
 import csv
 from os import remove
+
 from tree import BinarySearchTree
 from pais import Pais
+
 import random
 import os # módulo para acessar o terminal do sistema e poder fazer a limpeza
 
 tree = BinaryTree()
+country = Pais()
 
 dadosTemp = []
 dados = []
