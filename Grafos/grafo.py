10c7614453a1adbfe0ff69c4e780a72f3acb22d6
PomboObeso <matheus.gomes201@gmail.com>
2020-11-24 19:17:56 -0300

    Updates Comments Graph part 1

--- Grafos/grafo.py
+++ Grafos/grafo.py
@@ -1,23 +1,26 @@
+#Classe Vertex: Onde é montado nossos Vertices
 class Vertex:
+    #__init__ = Inicialzação das arestas setando o id e o vertice adjacente
     def __init__(self, node):
         self.id = node
         self.adjacent = {}
-
+    #__str__ = Retorna uma string mostrando os nós e arestas
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
-
+    #add_neighbor = O nó do grafo recebe um novo vizinho de peso 0
     def add_neighbor(self, neighbor, weight=0):
         self.adjacent[neighbor] = weight
-
+    #get_connections = retorna as chaves das arestas
     def get_connections(self):
         return self.adjacent.keys()  
-
+    #get_id = retorna o id do nó
     def get_id(self):
         return self.id
-
+    #get_weight = retorna o peso da aresta
     def get_weight(self, neighbor):
         return self.adjacent[neighbor]
 
+#Classe Graph = classe na qual é montado nosso grafo
 class Graph:
     def __init__(self):
         self.vert_dict = {}
